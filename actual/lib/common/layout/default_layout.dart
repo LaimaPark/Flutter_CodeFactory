bfb7fe6741e0a706c02e58a07cd18b2eae7e6221
Jaewon Park <laimaurora@gmail.com>
2023-08-14 19:24:01 +0900

    Flutter Lecture : Pagination - renderAppBar

--- actual/lib/common/layout/default_layout.dart
+++ actual/lib/common/layout/default_layout.dart
@@ -5,10 +5,12 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
+  final String? title;
 
   const DefaultLayout({
     required this.child,
     this.backgroundColor,
+    this.title,
     Key? key
   }) : super(key: key);
 
@@ -18,7 +20,22 @@ class DefaultLayout extends StatelessWidget {
     return Scaffold(
       // ?? = ?:
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
     );
   }
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0, /* 요즘은 없는게 트렌드 */
+        title: Text(
+          title!
+        )
+      );
+    }
+  }
+
 }

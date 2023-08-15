95de5d899b1a8aef7f30d04fad1019637bfd012e
Jaewon Park <laimaurora@gmail.com>
2023-08-15 15:34:36 +0900

    Flutter Lecture : Pagination - RestaurantCard 작업

--- actual/lib/restaurant/restaurant_screen.dart
+++ actual/lib/restaurant/restaurant_screen.dart
@@ -1,3 +1,4 @@
+import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantScreen extends StatelessWidget {
@@ -5,6 +6,22 @@ class RestaurantScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Container(child: Text('홈'),));
+    return Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: RestaurantCard(
+            image: Image.asset(
+              'asset/img/food/ddeok_bok_gi.jpg',
+              fit: BoxFit.cover,
+            ),
+            name: '불타는 떡볶이',
+            tags: ['떡볶이', '치즈', '매운맛'],
+            ratingCount: 100,
+            deliveryTime: 15,
+            deliveryFee: 2000,
+            rating: 100,
+          ),
+        )
+    );
   }
 }

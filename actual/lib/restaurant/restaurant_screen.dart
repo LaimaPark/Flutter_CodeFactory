556d1e8c80b68d6b676aeba848a56eaba6139f7d
Jaewon Park <laimaurora@gmail.com>
2023-08-16 21:09:18 +0900

    Flutter Lecture : Pagination - FutureBuilder

--- actual/lib/restaurant/restaurant_screen.dart
+++ actual/lib/restaurant/restaurant_screen.dart
@@ -1,27 +1,44 @@
+import 'package:actual/common/const/data.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
+  Future<List> paginateRestaurant() async {
+    final dio = Dio();
+
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    final resp = await dio.get('http://$ip/restaurant',
+        options: Options(headers: {'authorization': 'Bearer $accessToken'}));
+
+    return resp.data['data'];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: RestaurantCard(
-            image: Image.asset(
-              'asset/img/food/ddeok_bok_gi.jpg',
-              fit: BoxFit.cover,
-            ),
-            name: '불타는 떡볶이',
-            tags: ['떡볶이', '치즈', '매운맛'],
-            ratingCount: 100,
-            deliveryTime: 15,
-            deliveryFee: 2000,
-            rating: 100,
-          ),
-        )
-    );
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: FutureBuilder<List> (
+              future: paginateRestaurant(),
+              builder: (context, AsyncSnapshot<List> snapshot) {
+                return RestaurantCard(
+                  image: Image.asset(
+                    'asset/img/food/ddeok_bok_gi.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  name: '불타는 떡볶이',
+                  tags: ['떡볶이', '치즈', '매운맛'],
+                  ratingsCount: 100,
+                  deliveryTime: 15,
+                  deliveryFee: 2000,
+                  ratings: 100,
+                );
+              },
+            )
+        ));
   }
 }

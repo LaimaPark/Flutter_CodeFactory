7083486e639b4e8237e819eed8ed841faa0f78ce
Jaewon Park <laimaurora@gmail.com>
2023-08-21 20:57:05 +0900

    Flutter Lecture : Data Modeling - JSON Mapping

--- actual/lib/restaurant/restaurant_screen.dart
+++ actual/lib/restaurant/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:actual/common/const/data.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -24,8 +25,8 @@ class RestaurantScreen extends StatelessWidget {
     return Center(
         child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: FutureBuilder<List> (
-              future: paginateRestaurant(),
+            child: FutureBuilder<List>(
+              future: paginateRestaurant(), /* data 가져오는곳 */
               builder: (context, AsyncSnapshot<List> snapshot) {
                 if (!snapshot.hasData) {
                   /* Data 가 없으면?? */
@@ -33,37 +34,47 @@ class RestaurantScreen extends StatelessWidget {
                 }
 
                 return ListView.separated(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (_, index) {
-                      /* item 실행할때마다 생성댐 */
-                      final item = snapshot.data![index];
-
-                      return RestaurantCard(
-                        /* Network 로부터 이미지 가져옴 */
-                        image: Image.network(
-                          'http://$ip${item['thumUrl']}',
-                          fit: BoxFit.cover,
-                        ),
-                        // image: Image.asset(
-                        //   'asset/img/food/ddeok_bok_gi.jpg',
-                        //   fit: BoxFit.cover,
-                        // ),
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (_, index) {
+                    /* item 실행할때마다 생성댐 */
+                    final item = snapshot.data![index];
+                    // parsed
+                    final pItem = RestaurantModel(
+                        id: item['id'],
                         name: item['name'],
-                        tags: List<String>.from(item['tags']) /* Casting */,
+                        thumbUrl: 'http://$ip${item['thumbUrl']}',
+                        tags: List<String>.of(item['tags']),
+                        /* value 들을 하나씩 찾으면서 같은 값이 있는치 찾는 것 */
+                        priceRange: RestaurantPriceRange.values
+                            .firstWhere((e) => e.name == item['priceRange']),
+                        ratings: item['ratings'],
                         ratingsCount: item['ratingsCount'],
                         deliveryTime: item['deliveryTime'],
-                        deliveryFee: item['deliveryFee'],
-                        ratings: item['ratings'],
-                      );
-                    },
-                    separatorBuilder: (_, index) {
-                      /* 각 item 사이에 들어가는 공백이라고 보면 댐 */
-                      return SizedBox(height: 16.0,);
-                    },
-                );
+                        deliveryFee: item['deliveryFee']
+                    );
 
+                    return RestaurantCard(
+                      /* Network 로부터 이미지 가져옴 */
+                      image: Image.network(
+                        pItem.thumbUrl,
+                        fit: BoxFit.cover,
+                      ),
+                      name: pItem.name,
+                      tags: pItem.tags /* Casting */,
+                      ratingsCount: pItem.ratingsCount,
+                      deliveryTime: pItem.deliveryTime,
+                      deliveryFee: pItem.deliveryFee,
+                      ratings: pItem.ratings,
+                    );
+                  },
+                  separatorBuilder: (_, index) {
+                    /* 각 item 사이에 들어가는 공백이라고 보면 댐 */
+                    return SizedBox(
+                      height: 16.0,
+                    );
+                  },
+                );
               },
-            )
-        ));
+            )));
   }
 }

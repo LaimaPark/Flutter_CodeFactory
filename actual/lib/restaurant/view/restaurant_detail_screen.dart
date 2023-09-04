a194341d33fdff2fb115f88ab218fd3f15cca51a
Jaewon Park <laimaurora@gmail.com>
2023-09-04 22:12:31 +0900

    Flutter Lecture : SliverList

--- actual/lib/restaurant/view/restaurant_detail_screen.dart
+++ actual/lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:actual/common/layout/default_layout.dart';
 import 'package:actual/product/component/product_card.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
@@ -12,11 +14,40 @@ class RestaurantDetailScreen extends StatelessWidget {
         title: '불타는 떡볶이',
         child: CustomScrollView(
           slivers: [
-
+            renderTop(),
+            renderLabel(),
+            renderProducts()
           ],
         )
 
 
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),),
+      ),
+    );
+  }
+
+  renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+          delegate: SliverChildBuilderDelegate(
+                (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: ProductCard(),
+              );
+            },
+            childCount: 10,
+          )
+      ),
     );
   }
 

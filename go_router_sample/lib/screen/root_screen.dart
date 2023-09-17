291a65f4d7667809f52480e876fcf3851b31d26b
Jaewon Park <laimaurora@gmail.com>
2023-09-17 21:05:15 +0900

    Riverpod Sample : Default Settings

---
+++ go_router_sample/lib/screen/root_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:go_router_sample/layout/default_layout.dart';
+
+class RootScreen extends StatelessWidget {
+  const RootScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(body: ListView(children: [],));
+  }
+}
